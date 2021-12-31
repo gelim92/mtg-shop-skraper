383f8229b2f963d8b987d3523491b4d2826897a7
Gerard <gerard.lim92@gmail.com>
2021-12-31 16:21:39 +0800

    feat: setup material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Typography, AppBar, CssBaseline, Toolbar, Container } from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
 
-const App = () => {
+const App: FC = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <main>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">Photo Albumzzz</Typography>
+        </Toolbar>
+      </AppBar>
+      <div>
+        <Container maxWidth="sm">
+          <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+            Photo Album
+          </Typography>
+          <Typography variant="h5" align="center" color="textSecondary" paragraph>
+            Hello everyone this is a photo album and I&apos;m trying ot make this sentencde as long as possible so we
+            cans ee now does it look like on the screent
+          </Typography>
+        </Container>
+      </div>
+    </main>
   );
 };
 
